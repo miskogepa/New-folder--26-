9f523a80c32354bab4f342ccf700771fc3b7aa01
miskogepa <milovan.jankovic50m@gmail.com>
2025-07-04 23:03:43 +0200

    Update Header component to use framer-motion for logo animation; modify car data to include new images for Peugeot 308 GT and Toyota Camry.

--- bg/src/components/Header.jsx
+++ bg/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BlurText from "./BlurText";
+import { motion } from "framer-motion";
 import logo from "../assets/cars/logo.jpg";
 
 function Header() {
@@ -10,13 +11,13 @@ function Header() {
   return (
     <header className="bg-gray-500  py-8 ">
       <div className="container mx-auto px-4 flex justify-center items-center ">
-        <BlurText
-          text="Bela griva"
-          delay={150}
-          animateBy="words"
-          direction="top"
-          onAnimationComplete={handleAnimationComplete}
-          className="text-4xl font-bold text-center text-gray-600"
+        <motion.img
+          src={logo}
+          alt="Bela griva logo"
+          className="h-20 w-60 rounded-full shadow-lg  "
+          initial={{ filter: "blur(10px)", opacity: 0, y: -50 }}
+          animate={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
         />
       </div>
     </header>
--- bg/src/data/data.js
+++ bg/src/data/data.js
@@ -1,11 +1,14 @@
 import bmwImage from "../assets/cars/bmw.jpg";
+import zagImage from "../assets/cars/zagilogo.jpg";
+import bojkovicImage from "../assets/cars/bojkologo.jpg";
 
 export const cars = [
   {
     id: 1,
     owner: "Vladimir Petrović",
     model: "BMW X5",
-    image: "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?w=300&h=300&fit=crop"
+    image:
+      "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?w=300&h=300&fit=crop",
   },
   {
     id: 2,
@@ -17,9 +20,8 @@ export const cars = [
   {
     id: 3,
     owner: "Nikola Bojković",
-    model: "Audi A4",
-    image:
-      "https://images.unsplash.com/photo-1606664515524-ed2f786a0bd6?w=300&h=300&fit=crop",
+    model: "Peugeot 308 GT",
+    image: bojkovicImage,
   },
   {
     id: 4,
@@ -32,8 +34,7 @@ export const cars = [
     id: 5,
     owner: "Nemanja Zagorac",
     model: "Toyota Camry",
-    image:
-      "https://images.unsplash.com/photo-1621007947382-bb3c3994e3fb?w=300&h=300&fit=crop",
+    image: zagImage,
   },
   {
     id: 6,
