ca198185d8f595cbec6f3f26c3cc6b2aa3523fdb
miskogepa <milovan.jankovic50m@gmail.com>
2025-07-04 22:13:56 +0200

    Update package dependencies and refactor App component layout to include Hyperspeed background effect.

--- bg/src/App.jsx
+++ bg/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import CarGallery from "./components/CarGallery";
 import Header from "./components/Header";
+import Hyperspeed from "./Hyperspeed";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50  ">
-      <Header />
-      <main className="container mx-auto px-4">
-        <CarGallery />
-      </main>
+    <div className="relative min-h-screen w-full overflow-hidden">
+      {/* Pozadina */}
+      <div className="fixed inset-0 z-0 w-full h-full">
+        <Hyperspeed />
+      </div>
+      {/* Overlay sadržaj */}
+      <div className="relative z-10">
+        <Header />
+        <main className="container mx-auto px-4">
+          <CarGallery />
+        </main>
+      </div>
     </div>
   );
 }
